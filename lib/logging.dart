7e39413eb6ec087170c16e15b1fa71d2f2669b0b
Anaïs Betts <anais@anaisbetts.org>
2022-03-03 09:35:57 +0100

    Move logging to its own file

---
+++ lib/logging.dart
@@ -0,0 +1,69 @@
+import 'package:arquivolta/app.dart';
+import 'package:logger/logger.dart';
+
+abstract class Loggable {}
+
+abstract class CustomLoggable {
+  Logger get logger;
+}
+
+mixin LoggableMixin implements Loggable {
+  String? _klass;
+  Logger? _global;
+
+  void d(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _klass ??= runtimeType.toString();
+    _global ??= App.find<Logger>();
+
+    _global!.d('$_klass: $message', error, stackTrace);
+    if (this is CustomLoggable) {
+      (this as CustomLoggable).logger.d('$_klass: $message', error, stackTrace);
+    }
+  }
+
+  void i(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _klass ??= runtimeType.toString();
+    _global ??= App.find<Logger>();
+
+    _global!.i('$_klass: $message', error, stackTrace);
+    if (this is CustomLoggable) {
+      (this as CustomLoggable).logger.i('$_klass: $message', error, stackTrace);
+    }
+  }
+
+  void w(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _klass ??= runtimeType.toString();
+    _global ??= App.find<Logger>();
+
+    _global!.w('⚠️ $_klass: $message', error, stackTrace);
+    if (this is CustomLoggable) {
+      (this as CustomLoggable)
+          .logger
+          .w('⚠️ $_klass: $message', error, stackTrace);
+    }
+  }
+
+  void e(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _klass ??= runtimeType.toString();
+    _global ??= App.find<Logger>();
+
+    _global!.e('�� $_klass: $message', error, stackTrace);
+    if (this is CustomLoggable) {
+      (this as CustomLoggable)
+          .logger
+          .e('�� $_klass: $message', error, stackTrace);
+    }
+  }
+
+  void wtf(dynamic message, [dynamic error, StackTrace? stackTrace]) {
+    _klass ??= runtimeType.toString();
+    _global ??= App.find<Logger>();
+
+    _global!.wtf('�� _klass: $message', error, stackTrace);
+    if (this is CustomLoggable) {
+      (this as CustomLoggable)
+          .logger
+          .wtf('�� $_klass: $message', error, stackTrace);
+    }
+  }
+}

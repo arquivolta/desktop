4b32904cf7eae995d44dbc82678bb93adf8dd4eb
Anaïs Betts <anais@anaisbetts.org>
2022-03-07 13:41:28 +0100

    Implement the first crap version of the info prompt

--- lib/pages/install_page.dart
+++ lib/pages/install_page.dart
@@ -32,39 +32,121 @@ class InstallPage extends HookWidget
 
   @override
   Widget build(BuildContext context) {
-    final counter = useState(0);
+    final distroName = useRef('');
+    final username = useRef('');
+    final password = useRef('');
 
     final installResult = useAction(
       () async {
-        await installArchLinuxJob('arch-foobar').execute();
+        await installArchLinuxJob(distroName.value).execute();
 
         i('We did it!');
-        counter.value++;
       },
-      [
-        counter,
-      ],
+      [],
     );
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            'You have pushed the button this many times:',
+    final style = FluentTheme.of(context);
+    final headerText = installResult.isPending
+        ? 'Installing Arch Linux to ${distroName.value}'
+        : 'Install Arch Linux';
+
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Flex(
+        direction: Axis.vertical,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(headerText, style: style.typography.titleLarge),
+          Expanded(
+            child: installResult.isPending
+                ? InProgressInstall()
+                : Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 24),
+                    child: InstallPrompt(onPressedInstall: (d, u, p) {
+                      distroName.value = d;
+                      username.value = u;
+                      password.value = p;
+                      installResult.invoke();
+                    }),
+                  ),
           ),
-          Text(
-            '${counter.value}',
-          ),
-          if (installResult.isPending)
-            const ProgressRing()
-          else
-            Button(
-              onPressed: installResult.invoke,
-              child: const Text('Install Arch Linux'),
-            ),
         ],
       ),
     );
+  }
+}
+
+class InstallPrompt extends HookWidget implements Loggable {
+  final void Function(String distro, String user, String password)
+      onPressedInstall;
+
+  const InstallPrompt({Key? key, required this.onPressedInstall})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final distro = useTextEditingController(text: 'arch');
+    final user = useTextEditingController();
+    final password = useTextEditingController();
+    final passwordHidden = useState(true);
+
+    // Reevaluate shouldEnableButton whenever any of the text changes
+    [distro, user, password].forEach(useValueListenable);
+
+    final shouldEnableButton = distro.text.length > 1 &&
+        user.text.length > 1 &&
+        password.text.length > 1;
+
+    final onPress = useCallback(
+      () => onPressedInstall(
+        distro.text,
+        user.text,
+        password.text,
+      ),
+      [distro, user, password],
+    );
+
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        TextBox(
+          controller: distro,
+          header: 'WSL Distro Name',
+        ),
+        TextBox(
+          controller: user,
+          header: 'Linux Username',
+        ),
+        TextBox(
+          controller: password,
+          header: 'Password',
+          obscureText: passwordHidden.value,
+          suffix: IconButton(
+            icon: Icon(
+              passwordHidden.value ? FluentIcons.lock : FluentIcons.unlock,
+            ),
+            onPressed: () => passwordHidden.value = !passwordHidden.value,
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: FilledButton(
+              onPressed: shouldEnableButton ? onPress : null,
+              child: const Text('Install'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class InProgressInstall extends HookWidget implements Loggable {
+  @override
+  Widget build(BuildContext context) {
+    //final style = FluentTheme.of(context);
+    return Container();
   }
 }
